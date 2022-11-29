19ae858ec0a558358811f9f2239b5c7dbb188101
Jackson Zamorano <jackson@jacksonzamorano.com>
2022-11-29 10:23:19 -0700

    Started on wordlist
    Signed-off-by: Jackson Zamorano <jackson@jacksonzamorano.com>

---
+++ WordList/WordList/BinaryNode.hpp
@@ -0,0 +1,13 @@
+#ifndef BinaryNode_hpp
+#define BinaryNode_hpp
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class BinaryNode {
+    string key;
+};
+
+#endif
---
+++ WordList/WordList/BinaryTree.hpp
@@ -0,0 +1,10 @@
+#ifndef BinaryTree_hpp
+#define BinaryTree_hpp
+
+#include <stdio.h>
+
+class BinaryTree {
+
+};
+
+#endif
